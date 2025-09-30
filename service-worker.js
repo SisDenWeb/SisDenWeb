84e4b70eae8906f9ebbd7f00b21af50698d2b95f
Matheus Fernandes <matheus.fernandes92.235@protonmail.com>
2025-09-30 15:15:22 -0300

    WIP: PWA

---
+++ service-worker.js
@@ -0,0 +1,35 @@
+const CACHE_NAME = "SysDenWeb";
+const urlsToCache = [
+  "/",
+  "/source/html/index.html",
+];
+
+// Instala o service worker e faz cache dos arquivos
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Responde com cache quando possível
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+// Atualiza cache quando versão mudar
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.filter(name => name !== CACHE_NAME)
+                  .map(name => caches.delete(name))
+      );
+    })
+  );
+});
